d0f32774b3e1e07fdc31ed3f9855cfa60bf49679
gh <812704914@qq.com>
2023-02-16 02:15:21 +0800

    更新vite配置文件(打包显示空白)

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    proxy:{
+      'api':{
+        target:'https://gitee.com/GUANGHUIJs/ghstudy/blob/',
+        changeOrigin:true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      }
+    }
+  },
+  base:'./',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
